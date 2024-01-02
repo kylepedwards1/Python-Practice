dc7cddd688e32c889a3f95d4eb86f61b0dee85b6
Kyle Edwards <businesskylepedwards@outlook.com>
2024-01-02 15:43:48 -0600

    Project Completed

--- Hangman/hangman_game.py
+++ Hangman/hangman_game.py
@@ -1,7 +1,7 @@
 import os
 import random
 from hangman_words import word_list
-from hangman_art import logo
+from hangman_art import logo, stages
 
 
 def clear_terminal_screen():
@@ -11,6 +11,42 @@ def clear_terminal_screen():
 def hangman_game():
     clear_terminal_screen()
     print(logo)
+
+    chosen_word = random.choice(word_list)
+    word_length = len(chosen_word)
+    end_of_game = False
+    lives = 6
+
+    display = []
+
+    for _ in range(word_length):
+        display += "_"
+    
+    while not end_of_game:
+        guess = input("Guess a letter: ").lower()
+        
+        if guess in display:
+            print(f"You've already guessed {guess}.")
+        
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+        
+        if guess not in chosen_word:
+            print(f"You guessed {guess}. That's not in the word. You lost a life.")
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+                print(f"You lose. It was {chosen_word}.\n")
+        pass
+        print(f"{' '.join(display)}")
+
+        if "_" not in display:
+            end_of_game = True
+            print("You win.")
+
+        print(stages[lives])
 
 
 hangman_game()
